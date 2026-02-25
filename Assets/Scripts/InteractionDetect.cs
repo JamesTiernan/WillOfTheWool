a7fda74d2c007b28995b37aa5363d729032d4d45
Einiuss <Sadauskas-E@ulster.ac.uk>
2026-02-25 16:25:07 +0000

    NPCINTERACT

---
+++ Assets/Scripts/InteractionDetect.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
+
+public class InteractionDetect : MonoBehaviour
+{
+    private IInteractable InteractableInRange = null;
+    public GameObject InteractIcon;
+
+
+    void Start()
+    {
+        InteractIcon.SetActive(false);
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            InteractableInRange?.Interact();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger hit: " + collision.gameObject.name);
+        if (collision.TryGetComponent(out IInteractable interactable) && interactable.IsInteractable())
+        {
+            InteractableInRange = interactable;
+            InteractIcon.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out IInteractable interactable) && interactable == InteractableInRange)
+        {
+            InteractableInRange = null;
+            InteractIcon.SetActive(false);
+        }
+    }
+}
