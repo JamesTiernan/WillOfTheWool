8b0e152d46bf0ed49fd6a8f10fc47df85bc7075a
Einiuss <Sadauskas-E@ulster.ac.uk>
2026-03-03 15:11:18 +0000

    Eagle

--- Assets/Scripts/EnemyDetect.cs
+++ Assets/Scripts/EnemyDetect.cs
@@ -1,22 +1,40 @@
+using System;
 using UnityEngine;
 
 public class EnemyDetect : MonoBehaviour
 {
+    [SerializeField] float t;
+    private Animator animator;
+    bool attacking = false;
+    float attack = 0;
     void Start()
     {
-        
-
+        animator = GetComponent<Animator>();
     }
 
-   private void OnTriggerEnter2D(Collider2D other)
+    void Update()
     {
-        if (other.CompareTag("Player"))
+        if (attacking)
         {
-            GetComponentInChildren<EnemySideways>().StartSwoop(other.gameObject.transform);
-
+            attack = Mathf.Lerp(attack, 1f, t * Time.deltaTime);
         }
-       
-       
-        
+        else
+        {
+            attack = Mathf.Lerp(attack, 0f, t * Time.deltaTime);
+        }
+        animator.SetFloat("Attack",attack);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Is in range");
+        if (other.CompareTag("Player"))
+            attacking = true;
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Out of range");
+        if (other.CompareTag("Player"))
+            attacking = false;
     }
 }
