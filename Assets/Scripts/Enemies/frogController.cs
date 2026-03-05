09de323e4b7e66e344145a879f2ebed1f0ad634b
James Tiernan <Tiernan-J1@ulster.ac.uk>
2026-03-05 14:07:09 +0000

    refined frog AI

--- Assets/Scripts/Enemies/frogController.cs
+++ Assets/Scripts/Enemies/frogController.cs
@@ -17,10 +17,6 @@ public class frogController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(playerDetectAttack.playerDetected)
-        {
-            animator.SetTrigger("Attack");
-        }
         if(playerDetectMovement.playerDetected && canMove)
         {
             canMove = false;
@@ -29,22 +25,20 @@ public class frogController : MonoBehaviour
             playerDetectMovement.playerDetected = false;
             Debug.Log("hop !");
         }
+        animator.SetBool("Attack",playerDetectAttack.playerDetected);
+        
     }
 
     void Hop()
     {
         animator.SetTrigger("Hop");
-        //animator.ResetTrigger("Hop");
     }
 
     public void Move()
     {
         canMove = true;
         animator.Play("Idle",animator.GetLayerIndex("Base Layer"));
-        int moveDist;
-        Debug.Log($"My Rotation: {transform.localEulerAngles.y}");
-        if(transform.localEulerAngles.y == 180){moveDist = -6;}else{moveDist = 6;}
-        transform.position = new Vector2(transform.position.x + moveDist,transform.position.y);
+        transform.position = new Vector2(transform.position.x + 6,transform.position.y);
         playerDetectMovement.enabled = true;
     }
 }
