09a598b7708f9e4365156450dfea7f884ac41b2f
Einiuss <Sadauskas-E@ulster.ac.uk>
2026-04-13 13:00:02 +0100

    Coins

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+
+    CoinManager coinManager;
+    private void Start()
+    {
+
+        coinManager = GameObject.FindGameObjectWithTag("CoinManager").GetComponent<CoinManager>();
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+            coinManager.CoinAdd(1);
+            Debug.Log("Coin Added");
+            Destroy(gameObject);
+        }
+    }
+
+}
