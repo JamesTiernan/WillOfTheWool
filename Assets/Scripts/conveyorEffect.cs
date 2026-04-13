4db9f1a18c72efc68ec4b08ac1e4c2adb534904b
JamesTiernan <tiernan-j1@ulster.ac.uk>
2026-04-13 02:55:00 +0100

    factory level, conveyors

---
+++ Assets/Scripts/conveyorEffect.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class conveyorEffect : MonoBehaviour
+{
+    public bool onConveyor = false;
+    public float speed;
+
+    Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(onConveyor)
+        {
+            rb.linearVelocityX = speed;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(onConveyor)
+        {
+            if (collision.gameObject.GetComponent<conveyorEffect>() != null)
+            {
+                collision.gameObject.GetComponent<conveyorEffect>().speed = speed;
+                collision.gameObject.GetComponent<conveyorEffect>().onConveyor = true;
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if(onConveyor)
+        {
+            if (collision.gameObject.GetComponent<conveyorEffect>() != null)
+            {
+                collision.gameObject.GetComponent<conveyorEffect>().onConveyor = false;
+            }
+        }
+    }
+}
