25ffc008a3608d50601f1ca940fff0c72ea69ed0
Einiuss <Sadauskas-E@ulster.ac.uk>
2026-04-16 20:00:45 +0100

    OpenShop

---
+++ Assets/Scripts/OpenShop.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OpenShop : MonoBehaviour, IInteractable
+{
+
+
+    [SerializeField] private GameObject ShopPanel;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Interact()
+    {
+        ShopPanel.SetActive(true);
+    }
+
+    public bool IsInteractable()
+    {
+        return true;
+    }
+
+    public void Close()
+    {
+        ShopPanel.SetActive(false);
+    }
+}
