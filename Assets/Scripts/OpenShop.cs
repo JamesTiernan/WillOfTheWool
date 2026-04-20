e831f4f38f522beeb3492f1bb736ea47f9fcc7b5
Einiuss <Sadauskas-E@ulster.ac.uk>
2026-04-20 12:14:35 +0100

    Shop

--- Assets/Scripts/OpenShop.cs
+++ Assets/Scripts/OpenShop.cs
@@ -8,9 +8,16 @@ public class OpenShop : MonoBehaviour, IInteractable
 
     [SerializeField] GameObject woolPrefab;
     [SerializeField] private GameObject ShopPanel;
+    private CoinManager coinManager;
+    int Total;
+    [SerializeField] Sprite magnetWool;
+    [SerializeField] Sprite fireWool;
+    [SerializeField] Sprite stickyWool;
+    bool isInteractable;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        coinManager = GameObject.FindGameObjectWithTag("CoinManager").GetComponent<CoinManager>();
         
     }
 
@@ -25,6 +32,7 @@ public class OpenShop : MonoBehaviour, IInteractable
         ShopPanel.SetActive(true);
     }
 
+
     public bool IsInteractable()
     {
         return true;
@@ -37,8 +45,48 @@ public class OpenShop : MonoBehaviour, IInteractable
 
     public void BuyWool()
     {
+        int Total = coinManager.Coins;
+
+        if(Total > 0)
+        {
         GameObject newObject = Instantiate(woolPrefab);
         newObject.transform.position = transform.position;
-        
+        coinManager.CoinRemove(1);
+        }
+    }
+
+        public void MagnetWool()
+    {
+        int Total = coinManager.Coins;
+        if(Total > 0)
+        {
+        GameObject newObject = Instantiate(woolPrefab);
+        newObject.transform.position = transform.position;
+        coinManager.CoinRemove(1);
+        newObject.GetComponent<SpriteRenderer>().sprite = magnetWool;
+        }
+    }
+
+     public void StickyWool()
+    {
+        int Total = coinManager.Coins;
+        if(Total > 0)
+        {
+        GameObject newObject = Instantiate(woolPrefab);
+        newObject.transform.position = transform.position;
+        coinManager.CoinRemove(1);
+        newObject.GetComponent<SpriteRenderer>().sprite = stickyWool;
+        }
+    }
+     public void FireWool()
+    {
+        int Total = coinManager.Coins;
+        if(Total > 0)
+        {
+        GameObject newObject = Instantiate(woolPrefab);
+        newObject.transform.position = transform.position;
+        coinManager.CoinRemove(1);
+        newObject.GetComponent<SpriteRenderer>().sprite = fireWool;
+        }
     }
 }
