9df6b8c7a8add1103e92720681171694ed3c12a3
James Tiernan <Tiernan-J1@ulster.ac.uk>
2026-04-21 10:59:35 +0100

    crab animation

--- Assets/Scripts/SFXPlayer.cs
+++ Assets/Scripts/SFXPlayer.cs
@@ -4,6 +4,7 @@ public class SFXPlayer : MonoBehaviour
 {
     [SerializeField] AudioClip[] sounds;
     AudioSource soundPlayer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
